38dd243aca52b89f58a791b5aaf3a7f746498860
Teddy Parker <tparker@physics.ucsb.edu>
2017-06-09 02:45:23 -0700

    Data analysis

--- CustomTypes.jl
+++ CustomTypes.jl
@@ -12,3 +12,19 @@ end
 function SystemParameters()
   SystemParameters(0., 0., 0., 0., 0., 0, 0, 0)
 end
+
+type SystemSummary
+  params::SystemParameters
+  Ts::Vector{Float64}
+  avgEnergies::Vector{Float64}
+  energyVariances::Vector{Float64}
+  absAvgPsis::Vector{Float64}
+  avgAbsPsis::Vector{Float64}
+  absAvgPsi2s::Vector{Float64}
+  avgAbsPsi2s::Vector{Float64}
+  legendLabel::String
+end
+
+function SystemSummary(legendLabel::String)
+  SystemSummary(SystemParameters(), [], [], [], [], [], [], [], legendLabel)
+end
